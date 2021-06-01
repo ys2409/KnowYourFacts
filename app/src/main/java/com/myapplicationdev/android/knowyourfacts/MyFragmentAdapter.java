4f979b01a7492b36f62192a92e4d6e2bb313e475
19037610 <19037610@myrp.edu.sg>
2021-06-01 12:48:38 +0800

    completed MyFragmentAdapter

--- app/src/main/java/com/myapplicationdev/android/knowyourfacts/MyFragmentAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/knowyourfacts/MyFragmentAdapter.java
@@ -1,4 +1,26 @@
 package com.myapplicationdev.android.knowyourfacts;
 
-public class MyFragmentAdapter {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class MyFragmentAdapter extends FragmentPagerAdapter {
+    ArrayList<Fragment> fragments;
+
+    public MyFragmentAdapter(FragmentManager fm, ArrayList<Fragment> al) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragments = al;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
 }
